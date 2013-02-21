cf5ab3f08d446518875a52e4beb0a0eaffc2afc9
Christopher Best <bestchris@gmail.com>
2013-02-21 14:15:27 -0500

    enabled cursor noise

--- targeting.lisp
+++ targeting.lisp
@@ -210,7 +210,7 @@
 
 (define-model simple-tracking
 
-   (sgp :v t :needs-mouse nil :show-focus t :trace-detail high :cursor-noise nil :vwt t :incremental-mouse-moves t)
+   (sgp :v t :needs-mouse nil :show-focus t :trace-detail high :cursor-noise t :vwt t :incremental-mouse-moves t)
    (chunk-type targeting state target-x target-y cursor-diff-x cursor-diff-y target-location)
 
    (add-dm (track isa chunk) (attend-letter isa chunk)
