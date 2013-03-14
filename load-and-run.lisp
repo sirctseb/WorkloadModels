6cddd3b0eeec8558603aeee5099b3daf198a755f
Christopher Best <bestchris@gmail.com>
2013-03-14 19:46:49 -0400

    accept target projection and whiff wait time values on the command line

--- load-and-run.lisp
+++ load-and-run.lisp
@@ -3,6 +3,9 @@
 (load "~/Documents/actr6/load-act-r-6.lisp")
 ;; load model
 (load "~/Desktop/addition/targeting.lisp")
+;; set projection parameters if passed on command line
+(when (> (length ccl:*unprocessed-command-line-arguments*) 0) (setf *target-projection* (parse-integer (car ccl:*unprocessed-command-line-arguments*))))
+(when (> (length ccl:*unprocessed-command-line-arguments*) 1) (setf *whiff-wait-time* (parse-integer (cadr ccl:*unprocessed-command-line-arguments*))))
 ;; run trials
 (run-trials :trials 100 :moving t :visible nil :trace-file "trace.txt" :trace nil)
 ;; quit
