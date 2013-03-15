0f0725fee37d11e87ff6b8a4c5207c3211f7fcbb
Christopher Best <bestchris@gmail.com>
2013-03-15 12:40:17 -0400

    run for 1000 iterations

--- load-and-run.lisp
+++ load-and-run.lisp
@@ -7,6 +7,6 @@
 (when (> (length ccl:*unprocessed-command-line-arguments*) 0) (setf *target-projection* (parse-integer (car ccl:*unprocessed-command-line-arguments*))))
 (when (> (length ccl:*unprocessed-command-line-arguments*) 1) (setf *whiff-wait-time* (parse-integer (cadr ccl:*unprocessed-command-line-arguments*))))
 ;; run trials
-(run-trials :trials 100 :moving t :visible nil :trace-file "trace.txt" :trace nil)
+(run-trials :trials 1000 :moving t :visible nil :trace-file "trace.txt" :trace nil)
 ;; quit
 (quit)
