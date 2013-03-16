511036d75d623eca41287b6f0d9df8babaad637e
Christopher Best <bestchris@gmail.com>
2013-03-16 15:02:24 -0400

    reverted visible check removal commit

--- targeting.lisp
+++ targeting.lisp
@@ -10,6 +10,8 @@
 (defvar *default-screen-size* 400)
 ;; the default button size
 (defvar *default-button-size* 40)
+;; the current visibility of each button
+(defvar *buttons-visible* (make-hash-table))
 ;; the underlying colors of each button
 (defvar *button-colors* (make-hash-table))
 ;; the number of targets hit
@@ -117,6 +119,8 @@
   ;; action, so instead, schedule an event to remove the button in 0.01ms
   ;; set var that indicates a click occured more recently than manual request finish
   (setf *button-clicked* t)
+  ;; set var that shows this button is not visible
+  (setf (gethash b *buttons-visible*) nil)
   ;; schedule the event for actually removing the button
   (schedule-event-relative .001 (lambda() (remove-items-from-exp-window b) (proc-display)))
 )
@@ -129,6 +133,8 @@
     )))
     ; store buttons real color
     (setf (gethash button *button-colors*) (if enemy 'red 'green))
+    ; store that button is visible
+    (setf (gethash button *buttons-visible*) t)
     button
   )
 )
@@ -196,6 +202,7 @@
           (buttons (create-buttons num-targets button-size width height difficult))
           (returnvalue nil)
         )
+      ;(setf *buttons-visible* (make-list (list-length buttons) t))
     
       (if (not (subtypep (type-of window) 'virtual-window))
          (print-warning "This example only works correctly for virtual and visible-virtual windows because the x coordinate accessor is specific to those objects.")
@@ -215,6 +222,7 @@
                                             ;; Lisp since the x position accessor will differ.
                                               (dolist (button buttons)
     ;                                            (format t "seeing if button ~a is visible so we can move it" button)
+                                                (when (gethash button *buttons-visible*)
     ;                                              (format t "it is! moving it")
     											(when moving
                                                   (when show-motion (remove-items-from-exp-window button))
@@ -253,10 +261,11 @@
 	                                              )
 	                                            )
 												)
+                                                )
                                               )
-                                            (proc-display)
+                                            (proc-display))
                                        :details "moving object"
-                                       :initial-delay 0.5))
+                                       :initial-delay 0.5)
               )
             (cwd "/Users/sirc/Desktop/addition")
             (open-log-file)
