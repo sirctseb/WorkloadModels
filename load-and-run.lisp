6a25f735c3be39eb49ccb054e854cd4b2b0cf7ff
Christopher Best <bestchris@gmail.com>
2013-03-17 12:32:22 -0400

    fixed string comparison for arguments

--- load-and-run.lisp
+++ load-and-run.lisp
@@ -5,7 +5,7 @@
 (load "~/Desktop/addition/targeting.lisp")
 ;; set parameters if passed on command line
 (defvar trials 1000)
-(let* ((args ccl:*unprocessed-command-line-arguments*) (moving (if (eq (nth 0 args) "t") t nil)) (difficult (if (eq (nth 1 args) "t") t nil)))
+(let* ((args ccl:*unprocessed-command-line-arguments*) (moving (if (equal (nth 0 args) "t") t nil)) (difficult (if (equal (nth 1 args) "t") t nil)))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 2) (setf trials (parse-integer (nth 2 ccl:*unprocessed-command-line-arguments*))))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 3) (setf *target-projection* (parse-integer (nth 3 ccl:*unprocessed-command-line-arguments*))))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 4) (setf *whiff-wait-time* (parse-integer (nth 4 ccl:*unprocessed-command-line-arguments*))))
