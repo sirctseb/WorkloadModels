9d96a2aec1d35f3eec0cc8544d78a8d7defbb6bf
Christopher Best <bestchris@gmail.com>
2013-04-04 15:58:57 -0400

    tabs to spaces

--- targeting-easy-fast.lisp
+++ targeting-easy-fast.lisp
@@ -1,196 +1,196 @@
 ;;; Implementation of the targeting task for the experiment in the easy - fast condition
 (define-model simple-tracking
-	(sgp :needs-mouse nil :show-focus t :trace-detail high :cursor-noise t :vwt t :incremental-mouse-moves 0.01 :randomize-time nil :visual-movement-tolerance 10 :pixels-per-inch 96 :viewing-distance 96)
-	(chunk-type targeting state target-x target-y projected-x projected-y)
-	(chunk-type friend-target x y x-diff y-diff)
+  (sgp :needs-mouse nil :show-focus t :trace-detail high :cursor-noise t :vwt t :incremental-mouse-moves 0.01 :randomize-time nil :visual-movement-tolerance 10 :pixels-per-inch 96 :viewing-distance 96)
+  (chunk-type targeting state target-x target-y projected-x projected-y)
+  (chunk-type friend-target x y x-diff y-diff)
 
-	(suppress-warnings (add-dm (track isa chunk) (attend-letter isa chunk)
-		(goal isa targeting state find-red-target)))
-	
-	;; adding this setting to the model will avoid the deleted chunk
-	;; warnings in the object tracking case.
-	;; (sgp :delete-visicon-chunks nil)
+  (suppress-warnings (add-dm (track isa chunk) (attend-letter isa chunk)
+    (goal isa targeting state find-red-target)))
+  
+  ;; adding this setting to the model will avoid the deleted chunk
+  ;; warnings in the object tracking case.
+  ;; (sgp :delete-visicon-chunks nil)
 
-	;; Rule to start searching for a target  
-	(P find-red-target
-		=goal>
-			ISA         targeting
-			state       find-red-target
-	==>
-		+visual-location>
-			ISA         visual-location
-			:attended   nil
-			kind        OVAL
-			color       red
-		=goal>
-			state       cap-first-location
-		 ;; reset timer
-		+temporal>
-			ISA           time
-	)
+  ;; Rule to start searching for a target  
+  (P find-red-target
+    =goal>
+      ISA         targeting
+      state       find-red-target
+  ==>
+    +visual-location>
+      ISA         visual-location
+      :attended   nil
+      kind        OVAL
+      color       red
+    =goal>
+      state       cap-first-location
+     ;; reset timer
+    +temporal>
+      ISA           time
+  )
 
-	;; Rule to capture the location of a target when there is no friend info
-	(P cap-first-location
-		=goal>
-			ISA           targeting
-			state         cap-first-location
+  ;; Rule to capture the location of a target when there is no friend info
+  (P cap-first-location
+    =goal>
+      ISA           targeting
+      state         cap-first-location
 
-		;; find vis loc
-		=visual-location>
-			ISA           visual-location
-			screen-x      =tx
-			screen-y      =ty
+    ;; find vis loc
+    =visual-location>
+      ISA           visual-location
+      screen-x      =tx
+      screen-y      =ty
 
-		;; make sure visual is free so we can request move-attention
-		?visual>
-			state         free
+    ;; make sure visual is free so we can request move-attention
+    ?visual>
+      state         free
 
-	==>
-		;; store location in goal
-		=goal>
-			target-x      =tx
-			target-y      =ty
-			state         lead-target
+  ==>
+    ;; store location in goal
+    =goal>
+      target-x      =tx
+      target-y      =ty
+      state         lead-target
 
-		!eval!          (format t "storing first target location: ~a, ~a~%" =tx =ty)
+    !eval!          (format t "storing first target location: ~a, ~a~%" =tx =ty)
 
-		;; search for same location
-		+visual-location>
-			ISA           visual-location
-			:nearest      =visual-location
-	)
+    ;; search for same location
+    +visual-location>
+      ISA           visual-location
+      :nearest      =visual-location
+  )
 
-	;; Rule to capture second location of the target after moving attention
-	(P lead-target
-		=goal>
-			ISA           targeting
-			state         lead-target
-			target-x      =tx
-			target-y      =ty
-		;; get the new location
-		=visual-location>
-			ISA           visual-location
-			screen-x      =sx
-			screen-y      =sy
-		;; get elapsed time
-		=temporal>
-			ISA           time
-			ticks         =elapsed-ticks
-	==>
-		!eval!          (format t "second target location: ~a, ~a~%" =sx =sy)
-		;; calculate x difference
-		!bind!          =x-diff (- =sx =tx)
-		!bind!          =y-diff (- =sy =ty)
-		;; project location
-		!bind!          =projected-x (+ =tx (* *target-projection* (/ =x-diff =elapsed-ticks)))
-		!bind!          =projected-y (+ =ty (* *target-projection* (/ =y-diff =elapsed-ticks)))
-		!eval!          (format t "x-diff: ~a~%" =x-diff)
-		!eval!          (format t "speed: ~a~%" (/ =x-diff =elapsed-ticks))
-		!eval!          (format t "projecting move from ~a to ~a by ~a ~%" =tx =projected-x (* *target-projection* (/ =x-diff =elapsed-ticks)))
-		!eval!          (format t "projecting at x: ~a y: ~a, ticks: ~a~%" =projected-x =projected-y =elapsed-ticks)
-		;; store projected location in visual location buffer
-		=visual-location>
-			screen-x      =projected-x
-			screen-y      =projected-y
-		;; and move to next state
-		;; TODO move move request here to speed up?
-		=goal>
-			state         move-cursor
+  ;; Rule to capture second location of the target after moving attention
+  (P lead-target
+    =goal>
+      ISA           targeting
+      state         lead-target
+      target-x      =tx
+      target-y      =ty
+    ;; get the new location
+    =visual-location>
+      ISA           visual-location
+      screen-x      =sx
+      screen-y      =sy
+    ;; get elapsed time
+    =temporal>
+      ISA           time
+      ticks         =elapsed-ticks
+  ==>
+    !eval!          (format t "second target location: ~a, ~a~%" =sx =sy)
+    ;; calculate x difference
+    !bind!          =x-diff (- =sx =tx)
+    !bind!          =y-diff (- =sy =ty)
+    ;; project location
+    !bind!          =projected-x (+ =tx (* *target-projection* (/ =x-diff =elapsed-ticks)))
+    !bind!          =projected-y (+ =ty (* *target-projection* (/ =y-diff =elapsed-ticks)))
+    !eval!          (format t "x-diff: ~a~%" =x-diff)
+    !eval!          (format t "speed: ~a~%" (/ =x-diff =elapsed-ticks))
+    !eval!          (format t "projecting move from ~a to ~a by ~a ~%" =tx =projected-x (* *target-projection* (/ =x-diff =elapsed-ticks)))
+    !eval!          (format t "projecting at x: ~a y: ~a, ticks: ~a~%" =projected-x =projected-y =elapsed-ticks)
+    ;; store projected location in visual location buffer
+    =visual-location>
+      screen-x      =projected-x
+      screen-y      =projected-y
+    ;; and move to next state
+    ;; TODO move move request here to speed up?
+    =goal>
+      state         move-cursor
 
-		;; clear timer
-		+temporal>
-			ISA           clear
-	)
+    ;; clear timer
+    +temporal>
+      ISA           clear
+  )
 
-	;; rule to move cursor toward target
-	(P move-cursor
-		=goal>
-			ISA           targeting
-			state         move-cursor
+  ;; rule to move cursor toward target
+  (P move-cursor
+    =goal>
+      ISA           targeting
+      state         move-cursor
 
-		=visual-location>
-			ISA           visual-location
-			kind          OVAL
+    =visual-location>
+      ISA           visual-location
+      kind          OVAL
 
-		;; make sure motor system is free
-		?manual>
-			preparation   free
+    ;; make sure motor system is free
+    ?manual>
+      preparation   free
 
-		;; make sure visual is free
-		?visual>
-			state         free
-	==>
+    ;; make sure visual is free
+    ?visual>
+      state         free
+  ==>
 
-		;; request to move the cursor
-		+manual>
-			ISA           move-cursor
-			loc           =visual-location
-		;; request to attend to visual object so that we can search for nearest when
-		;; distinguishing between friend and enemy targets
-		; TODO it may be better to just keep the visual-location buffer full
-		; and supply that when making the new request in check-target
-		+visual>
-			ISA           move-attention
-			screen-pos    =visual-location
-		=goal>
-			state         distinguish-target
-	)
+    ;; request to move the cursor
+    +manual>
+      ISA           move-cursor
+      loc           =visual-location
+    ;; request to attend to visual object so that we can search for nearest when
+    ;; distinguishing between friend and enemy targets
+    ; TODO it may be better to just keep the visual-location buffer full
+    ; and supply that when making the new request in check-target
+    +visual>
+      ISA           move-attention
+      screen-pos    =visual-location
+    =goal>
+      state         distinguish-target
+  )
 
-	;; prepare a click while checking the target
-	(P prepare-click
-		=goal>
-			ISA           targeting
-			state         distinguish-target
+  ;; prepare a click while checking the target
+  (P prepare-click
+    =goal>
+      ISA           targeting
+      state         distinguish-target
 
-		;; wait until manual preparation is free and last command was a move (we didn't already prepare click)
-		?manual>
-			last-command  move-cursor
-			preparation   free
-	==>
-		;; prepare the mouse-click
-		;; TODO there is no prepare mouse-click, hopefully manually doing the punch right index works
-		+manual>
-			ISA           prepare
-			style         punch
-			hand          right
-			finger        index
-	)
+    ;; wait until manual preparation is free and last command was a move (we didn't already prepare click)
+    ?manual>
+      last-command  move-cursor
+      preparation   free
+  ==>
+    ;; prepare the mouse-click
+    ;; TODO there is no prepare mouse-click, hopefully manually doing the punch right index works
+    +manual>
+      ISA           prepare
+      style         punch
+      hand          right
+      finger        index
+  )
 
-	;; after a rescan of the target, check if the target is red and click it
-	(P distinguish-target-enemy
-		=goal>
-			ISA           targeting
-			state         distinguish-target
+  ;; after a rescan of the target, check if the target is red and click it
+  (P distinguish-target-enemy
+    =goal>
+      ISA           targeting
+      state         distinguish-target
 
-		;; let prepare-click go first
-		;; TODO this is not a semantic test. it only exists to allow prepare-click to go first
-		;; TODO there should be a better way to let prepare-click to have priority
-		;; TODO we could just put a flag in goal
-		?manual>
-			last-command  prepare
-			state         free
+    ;; let prepare-click go first
+    ;; TODO this is not a semantic test. it only exists to allow prepare-click to go first
+    ;; TODO there should be a better way to let prepare-click to have priority
+    ;; TODO we could just put a flag in goal
+    ?manual>
+      last-command  prepare
+      state         free
 
-		;; wait for visual to be free so we can clear it
-		?visual>
-			state         free
-	==>
-		=goal>
-			state         find-red-target
+    ;; wait for visual to be free so we can clear it
+    ?visual>
+      state         free
+  ==>
+    =goal>
+      state         find-red-target
 
-		;; submit click request
-		+manual>
-			;ISA           click-mouse
-			ISA           execute
+    ;; submit click request
+    +manual>
+      ;ISA           click-mouse
+      ISA           execute
 
-		;; clear visual buffer so that it doesn't keep re-encoding and slowing down future searches
-		+visual>
-			ISA           clear
+    ;; clear visual buffer so that it doesn't keep re-encoding and slowing down future searches
+    +visual>
+      ISA           clear
 
-		!eval!          (format t "detected enemy, clicking~%")
+    !eval!          (format t "detected enemy, clicking~%")
 
-		;; increment the number of targets checked
-		!eval!          (incf *check-order*)
-	)
+    ;; increment the number of targets checked
+    !eval!          (incf *check-order*)
+  )
 
-	(goal-focus goal)
+  (goal-focus goal)
 )
--- targeting-easy-slow.lisp
+++ targeting-easy-slow.lisp
@@ -1,129 +1,129 @@
 ;;; Implementation of the targeting task for the experiment in the easy - slow condition
 (define-model simple-tracking
 
-	(sgp :needs-mouse nil :show-focus t :trace-detail high :cursor-noise t :vwt t :incremental-mouse-moves 0.01 :randomize-time nil
-		:visual-movement-tolerance 0.5 :pixels-per-inch 96 :viewing-distance 96)
-	(chunk-type targeting state target-x target-y cursor-diff-x cursor-diff-y target-location)
-	(chunk-type friend-target x y)
+  (sgp :needs-mouse nil :show-focus t :trace-detail high :cursor-noise t :vwt t :incremental-mouse-moves 0.01 :randomize-time nil
+    :visual-movement-tolerance 0.5 :pixels-per-inch 96 :viewing-distance 96)
+  (chunk-type targeting state target-x target-y cursor-diff-x cursor-diff-y target-location)
+  (chunk-type friend-target x y)
 
-	(suppress-warnings (add-dm (track isa chunk) (attend-letter isa chunk)
-				(goal isa targeting state find-red-target)))
-	;; adding this setting to the model will avoid the deleted chunk
-	;; warnings in the object tracking case.
-	;; (sgp :delete-visicon-chunks nil)
+  (suppress-warnings (add-dm (track isa chunk) (attend-letter isa chunk)
+        (goal isa targeting state find-red-target)))
+  ;; adding this setting to the model will avoid the deleted chunk
+  ;; warnings in the object tracking case.
+  ;; (sgp :delete-visicon-chunks nil)
 
-	;; Rule to start searching for a target  
-	(P find-red-target
-		=goal>
-				ISA         targeting
-				state       find-red-target
-		?visual>
-				state       free
-	==>
-		+visual-location>
-				ISA         visual-location
-				:attended   nil
-				kind        OVAL
-				color       red
-		=goal>
-				state       move-cursor
-	)
+  ;; Rule to start searching for a target  
+  (P find-red-target
+    =goal>
+      ISA         targeting
+      state       find-red-target
+    ?visual>
+        state       free
+  ==>
+  +visual-location>
+      ISA         visual-location
+      :attended   nil
+      kind        OVAL
+      color       red
+    =goal>
+      state       move-cursor
+  )
 
-	(P on-move
-		=goal>
-				ISA         targeting
-				state       find-red-target
-		?visual-location>
-				state       error
-	==>
-		+visual-location>
-				ISA         visual-location
-				kind        OVAL
-		!eval!          (dolog "failed to attend to target location in find black target~%")
-		!eval!          (incf *vis-fails*)
-	)
-	(P on-move-move-cursor
-		=goal>
-			ISA           targeting
-			state         move-cursor
-		?visual-location>
-			state         error
-	==>
-		+visual-location>
-			ISA           visual-location
-			kind          OVAL
-		!eval!          (dolog "failed to attend to target location in move cursor~%")
-		!eval!          (incf *vis-fails*)
-	)
+  (P on-move
+    =goal>
+      ISA         targeting
+      state       find-red-target
+    ?visual-location>
+      state       error
+  ==>
+    +visual-location>
+      ISA         visual-location
+      kind        OVAL
+    !eval!          (dolog "failed to attend to target location in find black target~%")
+    !eval!          (incf *vis-fails*)
+  )
+  (P on-move-move-cursor
+    =goal>
+      ISA           targeting
+      state         move-cursor
+    ?visual-location>
+      state         error
+  ==>
+    +visual-location>
+      ISA           visual-location
+      kind          OVAL
+    !eval!          (dolog "failed to attend to target location in move cursor~%")
+    !eval!          (incf *vis-fails*)
+  )
 
-	;; rule to move cursor toward target
-	;; TODO can we somehow check that the imaginal buffer does NOT contain friend info?
-	;; TODO if we don't have such a check (like now), what happens? sometimes move-cursor goes
-	;; TODO even when friend info is there?
-	(P move-cursor
-		=goal>
-			ISA           targeting
-			state         move-cursor
+  ;; rule to move cursor toward target
+  ;; TODO can we somehow check that the imaginal buffer does NOT contain friend info?
+  ;; TODO if we don't have such a check (like now), what happens? sometimes move-cursor goes
+  ;; TODO even when friend info is there?
+  (P move-cursor
+    =goal>
+      ISA           targeting
+      state         move-cursor
 
-		=visual-location>
-			ISA           visual-location
-			kind          OVAL
-	;    screen-pos    =target-location
+    =visual-location>
+      ISA           visual-location
+      kind          OVAL
+  ;    screen-pos    =target-location
 
-		;; request to move cursor
-		;; TODO :cursor-noise should probably be enabled
-		;; TODO also, :default-target-width
-		;; TODO :incremental-mouse-moves?
-		;; TODO just look at all the parameters
+    ;; request to move cursor
+    ;; TODO :cursor-noise should probably be enabled
+    ;; TODO also, :default-target-width
+    ;; TODO :incremental-mouse-moves?
+    ;; TODO just look at all the parameters
 
-		;; make sure motor system is free
-		?manual>
-			preparation   free
-	==>
+    ;; make sure motor system is free
+    ?manual>
+      preparation   free
+  ==>
 
-		;; request to move the cursor
-		+manual>
-			ISA           move-cursor
-			;; TODO i guess we don't need the comparison rules
-			;; TODO do we even need to move visual attention to cursor?
-			;; TODO I think a better model is,
-			;; 1. find target
-			;; 2. move mouse
-			;; 3. while manual busy, track cursor
-			;; 4. if cursor within target, click button
-			loc           =visual-location
-		;; request to attend to visual object so that we can search for nearest when
-		;; distinguishing between friend and enemy targets
-		; TODO it may be better to just keep the visual-location buffer full
-		; and supply that when making the new request in check-target
-		+visual>
-			ISA           move-attention
-			screen-pos    =visual-location
-		=goal>
-			state         click-mouse
-	)
+    ;; request to move the cursor
+    +manual>
+      ISA           move-cursor
+      ;; TODO i guess we don't need the comparison rules
+      ;; TODO do we even need to move visual attention to cursor?
+      ;; TODO I think a better model is,
+      ;; 1. find target
+      ;; 2. move mouse
+      ;; 3. while manual busy, track cursor
+      ;; 4. if cursor within target, click button
+      loc           =visual-location
+    ;; request to attend to visual object so that we can search for nearest when
+    ;; distinguishing between friend and enemy targets
+    ; TODO it may be better to just keep the visual-location buffer full
+    ; and supply that when making the new request in check-target
+    +visual>
+      ISA           move-attention
+      screen-pos    =visual-location
+    =goal>
+      state         click-mouse
+  )
 
-	; request a mouse click
-	(P click-mouse
-		=goal>
-			ISA           targeting
-			state         click-mouse
+  ; request a mouse click
+  (P click-mouse
+    =goal>
+      ISA           targeting
+      state         click-mouse
 
-		;; wait until we attended the target
-		=visual>
-			ISA           OVAL
-		;; make sure motor module is free
-		?manual>
-			state         free
-	==>
-		
-		;; submit click request
-		+manual>
-			ISA           click-mouse
+    ;; wait until we attended the target
+    =visual>
+      ISA           OVAL
+    ;; make sure motor module is free
+    ?manual>
+      state         free
+  ==>
+    
+    ;; submit click request
+    +manual>
+      ISA           click-mouse
 
-		=goal>
-			state         find-red-target
-	)
+    =goal>
+      state         find-red-target
+  )
 
-	(goal-focus goal)
+  (goal-focus goal)
 )
