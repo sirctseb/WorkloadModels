bf644a318f0be05c085ecdce857bf270016349e9
Christopher Best <bestchris@gmail.com>
2013-05-15 11:56:14 -0400

    support non-integer values for alpha

--- load-and-run.lisp
+++ load-and-run.lisp
@@ -10,7 +10,7 @@
 (defvar trials 1000)
 (let* ((args ccl:*unprocessed-command-line-arguments*) (difficult (if (equal (nth 0 args) "t") t nil)) (moving (if (equal (nth 1 args) "t") t nil)))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 2) (setf trials (parse-integer (nth 2 ccl:*unprocessed-command-line-arguments*))))
-	(when (> (length ccl:*unprocessed-command-line-arguments*) 3) (setf *target-projection-alpha* (parse-integer (nth 3 ccl:*unprocessed-command-line-arguments*))))
+	(when (> (length ccl:*unprocessed-command-line-arguments*) 3) (setf *target-projection-alpha* (parse-number (nth 3 ccl:*unprocessed-command-line-arguments*))))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 4) (setf *target-projection-beta* (parse-number (nth 4 ccl:*unprocessed-command-line-arguments*))))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 5) (setf *whiff-wait-time* (parse-integer (nth 5 ccl:*unprocessed-command-line-arguments*))))
 	;; run trials
