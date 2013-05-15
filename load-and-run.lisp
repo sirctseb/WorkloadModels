85ebe1fef926acb72de14eb6fad62c2d247a8a41
Christopher Best <bestchris@gmail.com>
2013-05-15 11:51:12 -0400

    support non-integer values for beta

--- load-and-run.lisp
+++ load-and-run.lisp
@@ -4,12 +4,14 @@
 	(load "~/Documents/actr6/load-act-r-6.lisp"))
 ;; load model
 (load "targeting.lisp")
+;; load number parser
+(load "parse-number.lisp")
 ;; set parameters if passed on command line
 (defvar trials 1000)
 (let* ((args ccl:*unprocessed-command-line-arguments*) (difficult (if (equal (nth 0 args) "t") t nil)) (moving (if (equal (nth 1 args) "t") t nil)))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 2) (setf trials (parse-integer (nth 2 ccl:*unprocessed-command-line-arguments*))))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 3) (setf *target-projection-alpha* (parse-integer (nth 3 ccl:*unprocessed-command-line-arguments*))))
-	(when (> (length ccl:*unprocessed-command-line-arguments*) 4) (setf *target-projection-beta* (parse-integer (nth 4 ccl:*unprocessed-command-line-arguments*))))
+	(when (> (length ccl:*unprocessed-command-line-arguments*) 4) (setf *target-projection-beta* (parse-number (nth 4 ccl:*unprocessed-command-line-arguments*))))
 	(when (> (length ccl:*unprocessed-command-line-arguments*) 5) (setf *whiff-wait-time* (parse-integer (nth 5 ccl:*unprocessed-command-line-arguments*))))
 	;; run trials
 	(run-trials :trials trials :moving moving :difficult difficult :visible nil :trace-file "trace.txt" :trace nil))
