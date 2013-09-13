465ba7c20a9664cd7df2f616a6c3cb8a723b589b
Christopher Best <bestchris@gmail.com>
2013-09-13 11:07:53 -0400

    changed base levels

--- generateQNfile.pl
+++ generateQNfile.pl
@@ -75,8 +75,8 @@ if($addition) {
 				s/\*seq-base-level\*/0.1/g;
 				s/\*n-low-base-level\*/0.1/g;
 				s/\*n-high-base-level\*/0.0004/g;
-				s/\*a-no-carry-base-level\*/0.2/g;
-				s/\*a-carry-base-level\*/0.15/g;
+				s/\*a-no-carry-base-level\*/0.1/g;
+				s/\*a-carry-base-level\*/0.05/g;
 				# copy line to output file
 				print modelfile $_;
 			}
