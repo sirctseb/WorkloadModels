47536b8360848a9d0467d6b9f1bf2efe154e00ff
Christopher Best <bestchris@gmail.com>
2013-10-02 13:50:44 -0400

    add noise

--- generateQNfile.pl
+++ generateQNfile.pl
@@ -73,11 +73,11 @@ if($addition) {
 		if($model_def && !/; end model/) {
 			if(!badline($_)) {
 				# replace base level parameter variable with constants
-				s/\*seq-base-level\*/10/g;
-				s/\*n-low-base-level\*/3/g;
-				s/\*n-high-base-level\*/2/g;
-				s/\*a-no-carry-base-level\*/1/g;
-				s/\*a-carry-base-level\*/1/g;
+				s/\*seq-base-level\*/3/g;
+				s/\*n-low-base-level\*/4/g;
+				s/\*n-high-base-level\*/3/g;
+				s/\*a-no-carry-base-level\*/2/g;
+				s/\*a-carry-base-level\*/2/g;
 				# replace seed value with subject number
 				s/\(sgp :seed \(\d* \d*\)\)/(sgp :seed ($subject 1))/g;
 				# copy line to output file
